484c2c1143b529cd922567be86e5e8ce9a7f80dd
Garshin Oleg <olgarshin@gmail.com>
2020-02-08 23:55:21 +0700

    re

--- vito_technology/src/App.js
+++ vito_technology/src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import classes from './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Already modified</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
