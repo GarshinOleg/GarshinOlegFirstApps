7dbd9e58794489cd17135c1d36893b85dc0fd8fa
Garshin Oleg <olgarshin@gmail.com>
2020-02-09 00:03:10 +0700

    5

---
+++ vito_technology/build/precache-manifest.8f5ad0a9adc660412f5eb970d5a004bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2db8051b2feea94dac3723064caa8c94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8432bd2a19575726bd6b",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "46f3763ef02c2b1fe853",
+    "url": "/static/js/2.bf0d6427.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.bf0d6427.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8432bd2a19575726bd6b",
+    "url": "/static/js/main.0f001556.chunk.js"
+  },
+  {
+    "revision": "2225b4ef5de8e663e1bb",
+    "url": "/static/js/runtime-main.5901482b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
